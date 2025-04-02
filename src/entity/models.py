dfff65f1c3a5f30706a66bf52f0f06909bcd638e
Alexandrbig1 <alexsmagin1@gmail.com>
2025-04-02 16:39:43 -0500

    feat: add main file, add schemas, routes, services, repositories for contacts book. initialize with alembic. create fastapi. create docs for api as swagger

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,23 +1,25 @@
 from datetime import datetime
-from sqlalchemy import Integer, String, func, JSON
-from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
-from sqlalchemy.sql.sqltypes import DateTime
+
+from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.conf import constants
 
 
 class Base(DeclarativeBase):
+    pass
+
+
+class Contact_Book(Base):
+    __tablename__ = "Contact_Book"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(constants.USER_NAME_MAX_LENGTH), nullable=False)
+    surname: Mapped[str] = mapped_column(String(constants.USER_SURNAME_MAX_LENGTH), nullable=False)
+    email: Mapped[str] = mapped_column(String(constants.USER_EMAIL_MAX_LENGTH), nullable=False)
+    phone: Mapped[str] = mapped_column(String(constants.PHONE_NUMBER_MAX_LENGTH), nullable=False)
+    date_of_birth: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         DateTime, default=func.now(), onupdate=func.now()
     )
-
-
-class Contact(Base):
-    __tablename__ = "contacts"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
-    phone_number: Mapped[str] = mapped_column(String(100), nullable=False)
-    birthday: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    additional_data: Mapped[str] = mapped_column(String(150), nullable=False)
-    tags: Mapped[dict] = mapped_column(JSON, nullable=True)
